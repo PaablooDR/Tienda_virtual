565157d6d6afdedee408bcd1790dbbe749a5f368
PaablooDR <pab25702@gmail.com>
2023-11-30 12:04:13 +0100

    sidebar

--- views/admin/sidebar.php
+++ views/admin/sidebar.php
@@ -1,6 +1,8 @@
 <div id="sidebar">
-    <a href="index.php?controller=Admin&action=stadistics"><div>stadistics</div></a>
+    <div><img src="../../sources/logo.png" alt="logo"></div>
+    <a href="index.php?controller=Admin&action=stadistics"><div>Stadistics</div></a>
     <a href="index.php?controller=Admin&action=products"><div>Products</div></a>
     <a href="index.php?controller=Admin&action=categories"><div>Categories</div></a>
     <a href="index.php?controller=Admin&action=orders"><div>Orders</div></a>
+    <a href="index.php?controller=Admin&action=orders"><div>Log out</div></a>
 </div>
