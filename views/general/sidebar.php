e184f4f4728c32fd8e25aec117fa19029f68e434
PaablooDR <pab25702@gmail.com>
2023-11-30 10:43:12 +0100

    carpetas

---
+++ views/general/sidebar.php
@@ -0,0 +1,6 @@
+<div id="sidebar">
+    <a href="index.php?controller=Admin&action=stadistics"><div>stadistics</div></a>
+    <a href="index.php?controller=Admin&action=products"><div>Products</div></a>
+    <a href="index.php?controller=Admin&action=categories"><div>Categories</div></a>
+    <a href="index.php?controller=Admin&action=orders"><div>Orders</div></a>
+</div>
