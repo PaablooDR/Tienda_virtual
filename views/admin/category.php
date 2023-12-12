fe8a52e614b089b5a0661c7e98cae9a6a8b104b7
lucasmoreno12345 <lucas.moreno@inslapineda.cat>
2023-12-12 09:39:47 +0100

    edit category

--- views/admin/category.php
+++ views/admin/category.php
@@ -8,7 +8,7 @@ echo "<form action='index.php?controller=Admin&action=desactivateCategory' metho
         echo "<div><input type='checkbox' name='selectedItems[]' value='".$category['code']."'></div>";
         echo "<div>".$category['code']."</div>";
         echo "<div>".$category['name']."</div>";
-        echo "<a href='index.php?controller=Admin&action=editProduct&code=".$category['code']."'><div><img src='sources/edit.png' style='width: 150px;'></div></a>";
+        echo "<a href='index.php?controller=Admin&action=editCategory&code=".$category['code']."'><div><img src='sources/edit.png' style='width: 150px;'></div></a>";
     }
     echo "<input type='submit' name='desactivate' value='Change active'>";
 echo "</form>";
