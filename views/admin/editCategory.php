e6891153e2ea8dfab898d944ca3aa6fa425d9ae7
lucasmoreno12345 <lucas.moreno@inslapineda.cat>
2023-12-12 12:18:19 +0100

    edit category

--- views/admin/editCategory.php
+++ views/admin/editCategory.php
@@ -9,7 +9,7 @@
         <div>
             <!-- <?php print_r($data); ?> -->
             <form action="index.php?controller=Admin&action=updateCategory" method="post" enctype="multipart/form-data">
-                Code:<input type="text" name="code" placeholder="Name" value=<?php echo "$data[0]"; ?> maxlength="100" disabled><br>
+                Code:<input type="text" name="code" placeholder="Code" value=<?php echo "$data[0]"; ?> maxlength="100" disabled><br>
                 Name:<input type="text" name="name" placeholder="Name" value=<?php echo "$data[1]"; ?> maxlength="100" required><br>
                 <input type="submit" name="send" value="Update">
             </form>
