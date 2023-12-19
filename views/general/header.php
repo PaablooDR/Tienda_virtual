d035bf096192b8e97f599454fe6ac05bc3ec4191
lucasmoreno12345 <lucas.moreno@inslapineda.cat>
2023-12-19 13:58:23 +0100

    header

--- views/general/header.php
+++ views/general/header.php
@@ -4,5 +4,8 @@
         <a href="index.php?controller=Product&action=products"><li>Products</li></a>
         <a href="index.php?controller=Category&action=categories"><li>Categories</li></a>
         <a href="index.php?controller=PlateArt&action=plateart"><li>What's a PlateArt</li></a>
+        <li><input type="text" placeholder="Search for a theme..."></li>
+        <li><img src="sources/web/cart.png" alt="shopping cart" style="width: 150px;"></li>
+        <li><img src="sources/web/user.png" alt="user" style="width: 150px;"></li>
     </ul>
 </div>
