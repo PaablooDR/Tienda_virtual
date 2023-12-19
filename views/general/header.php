c723d62a8a08a86be7890c90bf50a50c56194b02
lucasmoreno12345 <lucas.moreno@inslapineda.cat>
2023-12-19 13:13:46 +0100

    header

---
+++ views/general/header.php
@@ -0,0 +1,8 @@
+<div id="header">
+    <ul>
+        <li><img src="sources/web/logo.png" alt="logo" style="width: 150px;"></li>
+        <a href="index.php?controller=Product&action=products"><li>Products</li></a>
+        <a href="index.php?controller=Category&action=categories"><li>Categories</li></a>
+        <a href="index.php?controller=PlateArt&action=plateart"><li>What's a PlateArt</li></a>
+    </ul>
+</div>
