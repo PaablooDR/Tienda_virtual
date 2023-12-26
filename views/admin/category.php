8e2b80a8ce6d37658dd7c75ea25db63cb24738fa
PaablooDR <pab25702@gmail.com>
2023-12-26 13:59:20 +0100

    css

--- views/admin/category.php
+++ views/admin/category.php
@@ -4,14 +4,28 @@
 
 <a href="index.php?controller=Category&action=new"><h3>New Category</h3></a>
 
-<?php
-echo "<form action='index.php?controller=Category&action=desactivate' method='post' enctype='multipart/form-data'>";
-    foreach($categories as $category) {
-        echo "<div><input type='checkbox' name='selectedItems[]' value='".$category['code']."'></div>";
-        echo "<div>".$category['code']."</div>";
-        echo "<div>".$category['name']."</div>";
-        echo "<a href='index.php?controller=Category&action=edit&code=".$category['code']."'><div><img src='sources/web/edit.png' style='width: 150px;'></div></a>";
-    }
-    echo "<input type='submit' name='desactivate' value='Change active'>";
-echo "</form>";
-?>
+<div id="adminContent">
+    <form action='index.php?controller=Category&action=desactivate' method='post' enctype='multipart/form-data'>
+        <div class="list">
+            <div>Select all</div>
+            <div>Code</div>
+            <div>Name</div>
+            <div>Edit</div>
+        </div>
+        <?php
+        foreach ($categories as $category) {
+        ?>
+            <div class="list">
+                <div> <input type='checkbox' name='selectedItems[]' value='<?php echo $category['code']; ?>'> </div>
+                <div><?php echo $category['code']; ?></div>
+                <div><?php echo $category['name']; ?></div>
+                <a href='index.php?controller=Category&action=edit&code=<?php echo $category['code']; ?>'>
+                    <div><img src='sources/web/edit.png' style='width: 150px;'></div>
+                </a>
+            </div>
+        <?php
+        }
+        ?>
+        <input type='submit' name='desactivate' value='Change active'>
+    </form>
+</div>
--- views/admin/product.php
+++ views/admin/product.php
@@ -1,24 +1,41 @@
 <!--Vista del admin a todo lo que puede hacer en productos-->
 
-<h1 id="titleAdmin">Products</h1>
+<div id="adminContent">
+    <h1 id="titleAdmin">Products</h1>
 
-<a href="index.php?controller=Product&action=new"><h3>New Product</h3></a>
+    <a href="index.php?controller=Product&action=new"><h3>New Product</h3></a>
 
-<?php
-echo "<form action='index.php?controller=Product&action=desactivate' method='post' enctype='multipart/form-data'>";
-    foreach($products as $product) {
-        echo "<div id='listProducts>";
-            echo "<div><input type='checkbox' name='selectedItems[]' value='".$product['code']."'></div>";
-            echo "<div><img src='" . $product['photo'] . "' alt='" . $product['name'] . "' style='width: 150px;'></div>";
-            echo "<div>".$product['name']."</div>";
-            echo "<div>".$product['code']."</div>";
-            echo "<div>".$product['category_name']."</div>";
-            echo "<div>".$product['price']."</div>";
-            echo "<div>".$product['stock']."</div>";
-            echo "<a href='index.php?controller=Product&action=edit&code=".$product['code']."&photo=".$product['photo']."'><div><img src='sources/web/edit.png' alt='edit' style='width: 50px;'></div></a>";
-        echo "</div>";
-    }
-    echo "<input type='submit' name='desactivate' value='Change active'>";
-echo "</form>";
 
-?>
+    <form action='index.php?controller=Product&action=desactivate' method='post' enctype='multipart/form-data'>
+        <div class="list">
+            <div>Select all</div>
+            <div>Photo</div>
+            <div>Name</div>
+            <div>Code</div>
+            <div>Category</div>
+            <div>Price</div>
+            <div>Stock</div>
+            <div>Edit</div>
+        </div>
+        <?php
+        foreach ($products as $product) { 
+        ?>
+            <div class='list'>
+                <div><input type='checkbox' name='selectedItems[]' value='<?php echo $product['code']; ?>'></div>
+                <div><img src='<?php echo $product['photo']; ?>' alt='<?php echo $product['name']; ?>' style='width: 150px;'></div>
+                <div><?php echo $product['name']; ?></div>
+                <div><?php echo $product['code']; ?></div>
+                <div><?php echo $product['category_name']; ?></div>
+                <div><?php echo $product['price']; ?></div>
+                <div><?php echo $product['stock']; ?></div>
+                <a href='index.php?controller=Product&action=edit&code=<?php echo $product['code']; ?>&photo=<?php echo $product['photo']; ?>'>
+                    <div><img src='sources/web/edit.png' alt='edit' style='width: 50px;'></div>
+                </a>
+            </div>
+        <?php 
+        } 
+        ?>
+        <input type='submit' name='desactivate' value='Change active'>
+    </form>
+
+</div>
