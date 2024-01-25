d870bbb870b738917b26c6e88049620dd13dee80
PaablooDR <pab25702@gmail.com>
2024-01-25 09:40:49 +0100

    header

--- views/general/header.php
+++ views/general/header.php
@@ -4,12 +4,19 @@
         <a href="index.php?controller=Product&action=products"><li>Products</li></a>
         <li class="dropdown">
             <a href="index.php?controller=Category&action=categories" class="dropbtn">Categories</a>
-<?php 
+<!-- <?php 
             $categoryController = new CategoryController();
             $categoryController->showCategories();
-?>
+?> -->
+            <ul class="dropdown-content">
+                <?php foreach ($categories as $category): ?>
+                    <li>
+                        <a href="index.php?controller=Product&action=singleCategory&id=<?php echo $category['code']; ?>"> <?php echo $category['name']; ?> </a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
         </li>
-        <a href="index.php?controller=PlateArt&action=plateart"><li>What's a PlateArt</li></a>
+        <a href="index.php?controller=Product&action=plateart"><li>What's a PlateArt</li></a>
         <li><input type="text" placeholder="Search for a theme..."></li>
         <li><img src="sources/web/cart.png" alt="shopping cart"></li>
         <a href="index.php?controller=User&action=login"><li><img src="sources/web/user.png" alt="user"></li></a>
