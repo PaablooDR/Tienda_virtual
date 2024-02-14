f23faba6cdff105d38f3c06947a3226d827bd96e
HugoDortoka <hugo.varela@inslapineda.cat>
2024-02-14 10:36:10 +0100

    vieworders

--- views/admin/viewOrder.php
+++ views/admin/viewOrder.php
@@ -2,28 +2,32 @@
     <?php
         require_once("views/admin/sidebar.php");
     ?>
+    <div id="invisibleSidebar"></div>
+    <div id="adminContent">
+        
+        <h1>Listado de Pedidos</h1>
 
-    <h1>Listado de Pedidos</h1>
+        <?php foreach ($pedidosConDetalles as $pedidoConDetalles): ?>
+            <h2>Pedido ID: <?php echo $pedidoConDetalles['pedido']['id_shopping']; ?></h2>
+            <p>Estat: <?php echo $pedidoConDetalles['pedido']['status']; echo "<a href= 'index.php?controller=Orders&action=editar&id=" . $pedidoConDetalles['pedido']['id_shopping'] . "' >Editar </a></p>"; ?>
+            <p>Data de Comanda: <?php echo $pedidoConDetalles['pedido']['shopping_date']; ?></p>
+            <!-- Mostrar más detalles del pedido según sea necesario -->
 
-    <?php foreach ($pedidosConDetalles as $pedidoConDetalles): ?>
-        <h2>Pedido ID: <?php echo $pedidoConDetalles['pedido']['id_shopping']; ?></h2>
-        <p>Estat: <?php echo $pedidoConDetalles['pedido']['status']; echo "<a href= 'index.php?controller=Orders&action=editar&id=" . $pedidoConDetalles['pedido']['id_shopping'] . "' >Editar </a></p>"; ?>
-        <p>Data de Comanda: <?php echo $pedidoConDetalles['pedido']['shopping_date']; ?></p>
-        <!-- Mostrar más detalles del pedido según sea necesario -->
+            <h3>Detalles del Pedido:</h3>
+            <ul>
+                <?php foreach ($pedidoConDetalles['detalles'] as $detalle): ?>
+                    <li>
+                        Código Producto: <?php echo $detalle['product']; ?> |
+                        Cantidad: <?php echo $detalle['amount']; ?> |
+                        Precio Total: <?php echo $detalle['total_price']; ?>€
+                    </li>
+                <?php endforeach; ?>
+            </ul>
 
-        <h3>Detalles del Pedido:</h3>
-        <ul>
-            <?php foreach ($pedidoConDetalles['detalles'] as $detalle): ?>
-                <li>
-                    Código Producto: <?php echo $detalle['product']; ?> |
-                    Cantidad: <?php echo $detalle['amount']; ?> |
-                    Precio Total: <?php echo $detalle['total_price']; ?>€
-                </li>
-            <?php endforeach; ?>
-        </ul>
+            <p>Precio Total del Pedido: <?php echo $pedidoConDetalles['pedido']['total_price']; ?>€</p>
+            <a href="index.php?controller=Orders&action=ticket&ticket=<?php echo $pedidoConDetalles['pedido']['id_shopping']; ?>"><button>Ticket</button></a>
+            <hr>
+        <?php endforeach; ?>
+    </div>
 
-        <p>Precio Total del Pedido: <?php echo $pedidoConDetalles['pedido']['total_price']; ?>€</p>
-        <a href="index.php?controller=Orders&action=ticket&ticket=<?php echo $pedidoConDetalles['pedido']['id_shopping']; ?>"><button>Ticket</button></a>
-        <hr>
-    <?php endforeach; ?>
 </div>
