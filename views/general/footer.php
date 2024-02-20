e5d7ac7d7dca2c9adbbcc018bfc61fdb5f724ac6
HugoDortoka <hugo.varela@inslapineda.cat>
2024-02-20 13:57:29 +0100

    scss

--- views/general/footer.php
+++ views/general/footer.php
@@ -1,18 +1,18 @@
 <div id="footer">
-    <ul>
-        <li><img src="sources/web/logo.png" alt="logo" style="width: 150px;"></li>
-        <li id="title">PlateArt</li>
-        <li>Find us
-            <ul id="icons">
-                <li><a href="https://www.instagram.com/" target="_blank"><img src="sources/web/instagram.png" alt="instagram" class="icon"></a></li>
-                <li><a href="https://twitter.com/?lang=ca" target="_blank"><img src="sources/web/twitter.png" alt="twitter" class="icon"></a></li>
-                <li><a href="https://www.tiktok.com/" target="_blank"><img src="sources/web/tiktok.png" alt="tiktok" class="icon"></a></li>
-            </ul>
-        </li>
-        <li>Secure payment
-            <ul>
-                <li><a href="https://www.paypal.com/es/home" target="_blank"><img src="sources/web/paypal.png" alt="paypal" id="paypal"></a></li>
-            </ul>
-        </li>
-    </ul>
+    <div>
+        <img src="sources/web/logo.png" alt="logo" style="width: 250px;">
+    </div>
+    <div id="title">PlateArt</div>
+    <div>Find us
+        <div id="icons">
+            <div><a href="https://www.instagram.com/" target="_blank"><img src="sources/web/instagram.png" alt="instagram" class="icon"></a></div>
+            <div><a href="https://twitter.com/?lang=ca" target="_blank"><img src="sources/web/twitter.png" alt="twitter" class="icon"></a></div>
+            <div><a href="https://www.tiktok.com/" target="_blank"><img src="sources/web/tiktok.png" alt="tiktok" class="icon"></a></div>
+        </div>
+    </div>
+    <div>Secure payment
+        <div>
+            <a href="https://www.paypal.com/es/home" target="_blank"><img src="sources/web/paypal.png" alt="paypal" id="paypal"></a>
+        </div>
+    </div>
 </div>
