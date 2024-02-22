749bc181b24222a70d76080515f1db849329b871
lucasmoreno12345 <lucas.moreno@inslapineda.cat>
2024-02-22 18:22:51 +0100

    full screen

--- views/admin/editOrder.php
+++ views/admin/editOrder.php
@@ -11,19 +11,21 @@ if (isset($_GET['id'])) {
 ?>
     <div id="invisibleSidebar"></div>
     <div id="adminContent">
-        <h1 id="titleAdmin">Edit status of order <?php echo $idPedido?></h1>
-        <form action="index.php?controller=Orders&action=actualizar" method="post">
-            <?php
-            echo "<input type='hidden' name='id_pedido' value='" . $idPedido . "'>"
-            ?>
+        <div class="fullScreen">
+            <h1 id="titleAdmin">Edit status of order <?php echo $idPedido?></h1>
+            <form action="index.php?controller=Orders&action=actualizar" method="post">
+                <?php
+                echo "<input type='hidden' name='id_pedido' value='" . $idPedido . "'>"
+                ?>
 
-            <label for="estado">Status:</label>
-            <select id="estado" name="estado">
-                <option value="pending">pending</option>
-                <option value="sent">sent</option>
-                <option value="cart">cart</option>
-            </select>
-            <input type="submit" value="Change status">
-        </form>
+                <label for="estado">Status:</label>
+                <select id="estado" name="estado">
+                    <option value="pending">pending</option>
+                    <option value="sent">sent</option>
+                    <option value="cart">cart</option>
+                </select>
+                <input type="submit" value="Change status">
+            </form>
+        </div>
     </div>
 </div>
--- views/admin/viewOrder.php
+++ views/admin/viewOrder.php
@@ -4,32 +4,32 @@
     ?>
     <div id="invisibleSidebar"></div>
         <div id="adminContent">
+            <div class="fullScreen">
+                <h1 id="titleAdmin">Order List</h1>
+                <?php foreach ($pedidosConDetalles as $pedidoConDetalles): ?>
+                    <div class="orderContainer">
+                        <h2>Order ID: <?php echo $pedidoConDetalles['pedido']['id_shopping']; ?></h2>
+                        <p>Status: <?php echo $pedidoConDetalles['pedido']['status']." "; echo "<a href= 'index.php?controller=Orders&action=editar&id=" . $pedidoConDetalles['pedido']['id_shopping'] . "' >Editar </a></p>"; ?>
+                        <p>Order Date: <?php echo $pedidoConDetalles['pedido']['shopping_date']; ?></p>
+                        <!-- Mostrar más detalles del pedido según sea necesario -->
+
+                        <h3>Order Details:</h3>
+                        <ul>
+                            <?php foreach ($pedidoConDetalles['detalles'] as $detalle): ?>
+                                <li>
+                                    Code product: <?php echo $detalle['product']; ?> |
+                                    Amount: <?php echo $detalle['amount']; ?> |
+                                    Total Price: <?php echo $detalle['total_price']; ?>€
+                                </li>
+                            <?php endforeach; ?>
+                        </ul>
+
+                        <p>Order Total Price: <?php echo $pedidoConDetalles['pedido']['total_price']; ?>€</p>
+                        <a href="index.php?controller=Orders&action=ticket&ticket=<?php echo $pedidoConDetalles['pedido']['id_shopping']; ?>"><button>Ticket</button></a>
+                    </div>     
+                <?php endforeach; ?>
+            </div>
             
-            <h1 id="titleAdmin">Order List</h1>
-
-            <?php foreach ($pedidosConDetalles as $pedidoConDetalles): ?>
-                <div class="orderContainer">
-                    <h2>Order ID: <?php echo $pedidoConDetalles['pedido']['id_shopping']; ?></h2>
-                    <p>Status: <?php echo $pedidoConDetalles['pedido']['status']." "; echo "<a href= 'index.php?controller=Orders&action=editar&id=" . $pedidoConDetalles['pedido']['id_shopping'] . "' >Editar </a></p>"; ?>
-                    <p>Order Date: <?php echo $pedidoConDetalles['pedido']['shopping_date']; ?></p>
-                    <!-- Mostrar más detalles del pedido según sea necesario -->
-
-                    <h3>Order Details:</h3>
-                    <ul>
-                        <?php foreach ($pedidoConDetalles['detalles'] as $detalle): ?>
-                            <li>
-                                Code product: <?php echo $detalle['product']; ?> |
-                                Amount: <?php echo $detalle['amount']; ?> |
-                                Total Price: <?php echo $detalle['total_price']; ?>€
-                            </li>
-                        <?php endforeach; ?>
-                    </ul>
-
-                    <p>Order Total Price: <?php echo $pedidoConDetalles['pedido']['total_price']; ?>€</p>
-                    <a href="index.php?controller=Orders&action=ticket&ticket=<?php echo $pedidoConDetalles['pedido']['id_shopping']; ?>"><button>Ticket</button></a>
-                </div>
-                        
-            <?php endforeach; ?>
         </div>
 
 </div>
