3b3bed6ea0762f065d93bd695b6f630b3e57b4bf
lucasmoreno12345 <lucas.moreno@inslapineda.cat>
2024-02-22 16:25:42 +0100

    order status

--- views/admin/editOrder.php
+++ views/admin/editOrder.php
@@ -5,17 +5,25 @@ if (isset($_GET['id'])) {
     echo "No se recibió id.";
 }
 ?>
+<div id="containerAdmin">
+<?php
+        require_once("views/admin/sidebar.php");
+?>
+    <div id="invisibleSidebar"></div>
+    <div id="adminContent">
+        <h1 id="titleAdmin">Edit status of order <?php echo $idPedido?></h1>
+        <form action="index.php?controller=Orders&action=actualizar" method="post">
+            <?php
+            echo "<input type='hidden' name='id_pedido' value='" . $idPedido . "'>"
+            ?>
 
-<form action="index.php?controller=Orders&action=actualizar" method="post">
-    <?php
-    echo "<input type='hidden' name='id_pedido' value='" . $idPedido . "'>"
-    ?>
-
-    <label for="estado">Estado:</label>
-    <select id="estado" name="estado">
-        <option value="pending">pending</option>
-        <option value="sent">sent</option>
-        <option value="cart">cart</option>
-    </select>
-    <input type="submit" value="Cambiar Estado">
-</form>
+            <label for="estado">Status:</label>
+            <select id="estado" name="estado">
+                <option value="pending">pending</option>
+                <option value="sent">sent</option>
+                <option value="cart">cart</option>
+            </select>
+            <input type="submit" value="Change status">
+        </form>
+    </div>
+</div>
