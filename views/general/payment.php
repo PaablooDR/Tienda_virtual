22c498955c57199ba2cbe8b7e286b30c8c1bc5b3
lucasmoreno12345 <lucas.moreno@inslapineda.cat>
2024-02-22 16:52:53 +0100

    payment

--- views/general/payment.php
+++ views/general/payment.php
@@ -1,20 +1,22 @@
 <div id="payment">
-    <div id="titleAdmin">
+    <div id="titleAdmin" class="titlePayment">
         <h1>Payment</h1>
     </div>
-    <div id="purchaseInformation">
-        <h3>Purchase information</h3>
-        <p>Import: <?php echo $totalPrice; ?> </p>
-        <p>Comerce: PlateArt</p>
-        <p>Order: <?php echo $order; ?> </p>
-        <p>Date: <?php echo $today; ?> </p>
-    </div>
-    <div id="paypal">
-        <h3>Paypal account</h3>
-        <form action="index.php?controller=Orders&action=orderPaid&order=<?php echo $order; ?>" autocomplete="off" method="post" enctype="multipart/form-data">
-            <input type="text" name="email" placeholder="Email" required><br>
-            <input type="password" name="password" placeholder="Password" required><br>
-            <input type="submit" name="send" value="Accept" aria-label="Pay">
-        </form>
+    <div id="paymentContainer">
+        <div id="purchaseInformation">
+            <h3>Purchase information</h3>
+            <p>Import: <?php echo $totalPrice; ?> </p>
+            <p>Comerce: PlateArt</p>
+            <p>Order: <?php echo $order; ?> </p>
+            <p>Date: <?php echo $today; ?> </p>
+        </div>
+        <div id="paypal">
+            <h3>Paypal account</h3>
+            <form action="index.php?controller=Orders&action=orderPaid&order=<?php echo $order; ?>" autocomplete="off" method="post" enctype="multipart/form-data">
+                <input type="text" name="email" placeholder="Email" required><br>
+                <input type="password" name="password" placeholder="Password" required><br>
+                <input type="submit" name="send" value="Accept" aria-label="Pay">
+            </form>
+        </div>
     </div>
 </div>
